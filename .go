ae4129154d0ff5ecf522bf8c46b0b0a2fadffabd
kashtanov <alexkashtanov@yahoo.ca>
2012-04-19 00:13:55 -0400

    add mango

--- .go
+++ .go
@@ -1,2 +1,3 @@
 go get github.com/hoisie/web
+go get github.com/paulbellamy/mango
 go install
--- app.go
+++ app.go
@@ -1,20 +1,48 @@
+// package main
+// 
+// import (
+//     "fmt"
+//     "log"
+//     "net/http"
+//     "os"
+// )
+// 
+// func main() {
+//     http.HandleFunc("/", hello)
+//     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+//     if err != nil {
+//         log.Fatal("ListenAndServe:", err)
+//     }
+// }
+// 
+// func hello(w http.ResponseWriter, req *http.Request) {
+//     fmt.Fprintln(w, "hello, esther!")
+// }
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+  "github.com/paulbellamy/mango"
+  "os"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+// Our default handler
+func Hello(env mango.Env) (mango.Status, mango.Headers, mango.Body) {
+	return 200, mango.Headers{}, mango.Body("Hello Esther!")
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(w, "hello, esther!")
+// Our handler for /goodbye
+func Goodbye(env mango.Env) (mango.Status, mango.Headers, mango.Body) {
+	return 200, mango.Headers{}, mango.Body("Goodbye Esther!")
+}
+
+func main() {
+	stack := new(mango.Stack)
+	stack.Address = ":"+os.Getenv("PORT")
+
+	// Route all requests for /goodbye to the Goodbye handler
+	routes := map[string]mango.App{"/goodbye(.*)": Goodbye}
+	stack.Middleware(mango.Routing(routes))
+
+	// Hello handles all requests not sent to Goodbye
+	stack.Run(Hello)
 }
